daad3ba0ff635272a577ae2a1fc88ee1f5f8d866
Jack <jackowendickson@gmail.com>
2022-03-30 14:05:09 +0100

    4 cinema done

--- models/cinema.js
+++ models/cinema.js
@@ -10,4 +10,18 @@ Cinema.prototype.getFilmTitles = function() {
   return result
 }
 
+Cinema.prototype.getFilmByTitle = function(title) {
+  const result = this.films.find((film) => {
+    return film.title == title;
+  })
+  return result;
+}
+
+Cinema.prototype.getFilmsByGenre = function(genre) {
+  const result = this.films.filter((film) => {
+    return film.genre == genre;
+  });
+  return result
+}
+
 module.exports = Cinema;
